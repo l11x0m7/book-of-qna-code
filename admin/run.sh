bd525e294c67e064f0ca8b229fe496be2007077e
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2018-05-28 00:25:15 -0700

    start docker container

---
+++ admin/run.sh
@@ -0,0 +1,17 @@
+#! /bin/bash 
+###########################################
+#
+###########################################
+
+# constants
+baseDir=$(cd `dirname "$0"`;pwd)
+# functions
+
+# main 
+[ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
+cd $baseDir/..
+docker run -it --rm \
+	-v $PWD:/app \
+	chatopera/qna-book:1.0.0 \
+	bash
+
